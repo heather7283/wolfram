9e2a863463b60bf699e6ccca64366fa2f4322b45
heather7283 <heather7283@protonmail.com>
2026-04-06 01:08:41 +0400

    better looking buttons

--- app/src/main/java/io/github/heather7283/wolfram/ui/configs/ConfigsScreen.kt
+++ app/src/main/java/io/github/heather7283/wolfram/ui/configs/ConfigsScreen.kt
@@ -3,10 +3,13 @@ package io.github.heather7283.wolfram.ui.configs
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -14,9 +17,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
@@ -27,6 +32,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.PullToRefreshState
 import androidx.compose.runtime.Composable
@@ -34,6 +40,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -53,27 +60,35 @@ fun ConfigEntry(
     config: XrayConfig,
     onClick: () -> Unit,
     onEdit: () -> Unit,
+    onDelete: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Card(onClick, modifier = modifier) {
+    Card(onClick = onClick, modifier = modifier) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
         ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    config.name,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-                Text(
-                    config.jsonPath.pathString,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-            }
-            Button(onEdit) {
+            Text(
+                text = config.name,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            FilledTonalButton(
+                onClick = onEdit,
+                // https://stackoverflow.com/a/66671903
+                modifier = Modifier.size(50.dp),
+                contentPadding = PaddingValues(0.dp),
+            ) {
                 Icon(Icons.Default.Edit, contentDescription = "Edit")
+            }
+            FilledTonalButton(
+                onClick = onDelete,
+                modifier = Modifier.size(50.dp),
+                contentPadding = PaddingValues(0.dp),
+            ) {
+                Icon(Icons.Default.DeleteForever, contentDescription = "Delete")
             }
         }
     }
@@ -117,7 +132,8 @@ fun ConfigsScreen(
                         },
                         onEdit = {
                             navActions.navigateToAddEditConfig("Edit config", it)
-                        }
+                        },
+                        onDelete = {},
                     )
                 }
             }
