afe15173065b751450cd054e1bb515709656bf1a
Heather <heather7283@protonmail.com>
2026-07-29 05:31:12 +0400

    feat(tile): update state based on vpn status

--- app/src/main/kotlin/io/github/heather7283/wolfram/tile/WolframTileService.kt
+++ app/src/main/kotlin/io/github/heather7283/wolfram/tile/WolframTileService.kt
@@ -2,26 +2,57 @@ package io.github.heather7283.wolfram.tile
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
+import dagger.hilt.android.AndroidEntryPoint
 import io.github.heather7283.wolfram.WolframActivity
+import io.github.heather7283.wolfram.data.config.XrayConfigRepository
+import io.github.heather7283.wolfram.data.geofile.GeoFileRepository
+import io.github.heather7283.wolfram.data.settings.SettingsRepository
+import io.github.heather7283.wolfram.data.xray.XrayRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class WolframTileService : TileService() {
+    @Inject lateinit var xrayRepository: XrayRepository
+
+    private var job: Job? = null
+
     // Called when the user adds your tile.
     override fun onTileAdded() {
         Timber.d("onTileAdded")
         super.onTileAdded()
     }
+
     // Called when your app can update your tile.
     override fun onStartListening() {
         Timber.d("onStartListening")
         super.onStartListening()
+
+        job = CoroutineScope(Dispatchers.Main).launch {
+            xrayRepository.running.collect { isRunning ->
+                Timber.d("collect fired: isRunning=${isRunning}")
+                qsTile.apply {
+                    state = if (isRunning) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+                    updateTile()
+                }
+            }
+        }
     }
 
     // Called when your app can no longer update your tile.
     override fun onStopListening() {
         Timber.d("onStopListening")
         super.onStopListening()
+
+        job?.cancel()
     }
 
     // Called when the user taps on your tile in an active or inactive state.
